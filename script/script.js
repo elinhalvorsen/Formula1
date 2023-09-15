dc357909c5eb2f8d9cf1c680a621942b33f06c65
Elin Halvorsen <elinhalvorsen@Elin-sin-MacBook-Air.local>
2023-09-15 19:38:45 +0200

    formula1

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+const carsArray = [
+    {
+      title: "Lego Ferrari",
+      image: "/egneProsjekter/Formula1/images/lego-ferrari.png",
+      description: "heihei",
+      button: ""
+    }
+  ];
+  
